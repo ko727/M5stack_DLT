48cb285f8b2a12ca4230972f42a723de34364454
ko727 <otsuka.kenya@nihon-u.ac.jp>
2024-09-17 18:40:40 +0900

    s

--- 1U/src/main.cpp
+++ 1U/src/main.cpp
@@ -1,6 +1,7 @@
 #include <ArduinoOTA.h>
 #include <M5Stack.h>
 #include <WiFi.h>
+#include <mcp_can.h>
 
 // WiFi credentials.
 // Set password to "" for open networks.
@@ -16,9 +17,103 @@ int wifi_cnt;
 int wifi_num = 1;
 bool OTA_flag = false;
 
+// Interrupt setup
+#define M5_Interrupt1 2
+#define M5_Interrupt2 5
+
+// I2C Encoder
+#define M5_SDA 21
+#define M5_SDL 22
+  //Encoder i2C
+#define ENCODER_ADDR 0x59
+  //write
+#define ENCODER_PULSE 0x50
+#define ENCODER_Z 0x70
+  //read
+#define ENCODER_VALUE 0x00
+#define ENCODER_TURNS 0x60
+
+int set_pulse = 2048;
+int set_Z = 0;
+int data_pulse[4];
+int data[4];
+int value;
+
+// CAN setup
+#define CAN_IDaddress 0x06A
+#define CAN0_INT 15   // Set INT to pin 15
+MCP_CAN CAN0(12);     // Set CS to pin 12
+int ds;
+int d0,d1,d2,d3;
+
+
+void init_can(){
+  // MCP2515の初期化に成功した場合（ビットレート500kb/s ）
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK){
+    // 特になにもしない    
+  }else{  // 初期化に失敗した場合
+    // 特になにもしない    
+  }
+  // MCP2515を通常モードに設定
+  CAN0.setMode(MCP_NORMAL);
+  
+  // CAN0_INTを入力ピンに設定（受信用）
+  pinMode(CAN0_INT, INPUT);
+}
+
+void sendData(){
+  d3 = ds & 0xFF;
+  d2 = ds >> 8 & 0xFF;
+  d1 = ds >> 16 & 0xFF;
+  d0 = ds >> 24 & 0xFF;
+  Serial.println(d1);
+  byte data[4] = {d0, d1, d2, d3};
+
+  byte sndStat = CAN0.sendMsgBuf(CAN_IDaddress, 1, 4, data);
+
+  delay(10);   // send data per 10ms
+}
+
+void IRAM_ATTR onRise1() {
+  ds = 0;
+}
+void IRAM_ATTR onRise2() {
+  ds = 0;
+}
+
 void setup() {
   M5.begin();
   M5.Power.begin();
+
+  //Interrupt set
+  pinMode(M5_Interrupt1,INPUT_PULLDOWN);
+  attachInterrupt(M5_Interrupt1,onRise1,RISING);
+  pinMode(M5_Interrupt2,INPUT_PULLDOWN);
+  attachInterrupt(M5_Interrupt2,onRise2,RISING);
+
+  //Set CAN
+  init_can();
+
+  //Encoder write
+  Wire.begin(M5_SDA,M5_SDL);
+  //pulse
+  Wire.beginTransmission(ENCODER_ADDR);
+  Wire.write(ENCODER_PULSE);
+  set_pulse = set_pulse*2;
+  data_pulse[0] = set_pulse & 0xFF;
+  data_pulse[1] = (set_pulse >> 8) & 0xFF;
+  data_pulse[2] = (set_pulse >> 16) & 0xFF;
+  data_pulse[3] = (set_pulse >> 24) & 0xFF;
+  Wire.write(data_pulse[0]);
+  Wire.write(data_pulse[1]);
+  Wire.write(data_pulse[2]);
+  Wire.write(data_pulse[3]);
+  Wire.endTransmission(true);
+  //Z
+  Wire.beginTransmission(ENCODER_ADDR);
+  Wire.write(ENCODER_Z);
+  Wire.write(set_Z);
+  Wire.endTransmission(true);
 
   //WiFi OTA
   M5.Lcd.setTextSize(2);
@@ -137,6 +232,22 @@ void loop() {
     }
     delay(50);
   }else{
-    delay(20);
+    //Read Encoder value
+    Wire.beginTransmission(ENCODER_ADDR);
+    Wire.write(ENCODER_PULSE);
+    Wire.endTransmission(true);
+    delay(10);
+    Wire.requestFrom(ENCODER_ADDR,4);
+    for (int i = 0; i < 4; i++){
+      data[i] = Wire.read();
+    }
+    value = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+
+    if (ds == 0) {
+      delay(1000);
+    }else{
+      sendData();
+    }
+    delay(10);
   }
 }
